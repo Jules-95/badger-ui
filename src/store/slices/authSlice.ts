0e79acd940618b95c2bd144a1ace071dd2ab4b74
Jules-95 <jlsmillet@yahoo.fr>
2026-05-27 09:57:34 +0200

    Verification de l'expiration du token au chargement

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -1,16 +1,24 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-// Etat initial avec token qui est récupérér depuis le local storage 
-
+// Décodage du token JWT 
 const decodeToken = (token: string) => {
-    const decoded = JSON.parse(atob(token.split('.')[1]));
-    return decoded.user; 
+    return JSON.parse(atob(token.split('.')[1])); 
 }
+
+// Vérification de l'expiration du token 
+const isTokenExpired = (token:string) => {
+    return Date.now() / 1000 > decodeToken(token).exp;
+}
+
+// Récupération du token depuis localStorage si il existe ET si il n'est pas expiré. 
+const saveToken = localStorage.getItem("token");
+const validToken = saveToken && !isTokenExpired(saveToken) ? saveToken : null;
+
 
 // Si il n'existe pas alors string vide
 const initialState: { token: string; user: any} = {
-    token: localStorage.getItem("token") || "",
-    user: localStorage.getItem("token") ? decodeToken(localStorage.getItem("token")!) : null
+    token: validToken || "",
+    user: validToken ? decodeToken(validToken).user : null
 };
 
 // C'est donc le slice qui va généré le token JWT
@@ -25,7 +33,7 @@ export const authSlice = createSlice({
         setToken: (state, action: PayloadAction<string>) => {
             state.token = action.payload;
             localStorage.setItem("token", action.payload);
-            state.user = decodeToken(action.payload);
+            state.user = decodeToken(action.payload).user;
         },
 
         // removeToken permet de supprimer le token du store et du local storage 
