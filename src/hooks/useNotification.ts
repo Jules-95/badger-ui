42b313809b6a79da8e3eb098816f8b6716d60e2f
Jules-95 <jlsmillet@yahoo.fr>
2026-05-29 10:21:32 +0200

    hook usenotification passé en TypeScript (Problèmes avec login en tsx)

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
+interface INotification {
+    message: string;
+    type: string;
+}
+
 // Hook personnalisé pour gérer les notifications
 export default function useNotification() {
-    const [notification, setNotification] = useState(null);
+    const [notification, setNotification] = useState<INotification | null>(null);
 
     // Affiche une notification qui disparaît après 5 secondes
-    const showMessage = (message, type) => {
+    const showMessage = (message: string, type: string) => {
         setNotification({ message, type });
         setTimeout(() => setNotification(null), 5000);
     };
