e882503b9fcf8f6b15dd3d3181e4c5c8a3cd2682
Jules-95 <jlsmillet@yahoo.fr>
2026-05-29 12:29:47 +0200

    Home page basique

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Bienvenue sur Badger en moins bien</h1>
+// pages/Home.jsx
 
-      <p> Affichage de données générales ici </p>
+import { useSelector } from "react-redux";
+
+export default function Home() {
+  const user = useSelector((state) => state.auth.user);
+
+  return (
+    <div className="page">
+
+      <div className="home-hero">
+        <h2>Bonjour, {user?.firstname} 👋</h2>
+        <p>Bienvenue sur Badger en moins bien. Utilisez la navbar pour naviguer.</p>
+      </div>
+
+      <div className="home-wip">
+        <p>Récap des données importantes ici <br/> En cours de développement</p>
+      </div>
+
     </div>
   );
 }
